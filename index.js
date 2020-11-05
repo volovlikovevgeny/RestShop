a7bfa475a6397d99e2c40d087136febafe76ee14
Evgeny Volovlikov <volovlikovevgeny@mail.ru>
2020-11-05 15:37:37 +0300

    finishing section styling,adding functionallity,upload img

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const menu = document.querySelector('.menu');
 const navOpen = document.querySelector('.hamburger');
 const navClose = document.querySelector('.close');
+const navLinks = document.querySelectorAll('.nav-item a');
 
 const navLeft = menu.getBoundingClientRect().left;
-
-
 
 navOpen.addEventListener('click', () => {
     if (navLeft < 0) {
@@ -17,6 +16,49 @@ navClose.addEventListener('click', () => {
         menu.classList.remove('show');
     }
 });
+
+// Fixed Nav 
+
+const nav = document.querySelector('.nav');
+const navHeight = nav.getBoundingClientRect().height;
+
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+
+    scrollHeight > navHeight ? nav.classList.add('fix-nav') : nav.classList.remove('fix-nav')
+})
+
+
+// Smooth-Scrolling Jquery
+
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        //Сохраняем значение атрибута href в переменной:
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top//можно вычесть высоту меню
+        }, 800);
+        return false;
+    });
+});
+
+// ScroolTo
+
+const links = [...document.querySelectorAll('.scroll-link')]
+
+links.map(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('show');
+    })
+})
+
+// AOS animate Scrolling
+
+AOS.init()
+
+
+
+
 
 
 
